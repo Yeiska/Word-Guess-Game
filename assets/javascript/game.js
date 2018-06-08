2ee49415ca28509bece0b57fe257123cc824f87a
Yeiska <yeiska83@yahoo.com>
2018-06-08 00:15:10 -0400

    still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,13 @@ window.onload = function () {
     var game = ["electricity", "current", "tension", "alternator", "battery"];
     var guesses = [];//Stored guesses
     var numberGuessesRemaining = 15;//count guesses
+    var guessesRemaining;
     var space;
-    var guessesRemaining = 5;
 
-    var elem = document.getElementById('content');
-    var child = elem.children[0];
-    child = elem.wordGuess;
+    //hidde word and show only length as spaces!!!
+    var space = document.getElementById("space");
+    space += wordGuess;
+    space.innerHTML;
 
     //Seleccting word to quess from array game
     var wordToGuess = game[Math.floor(Math.random() * game.length)];
@@ -17,62 +18,44 @@ window.onload = function () {
     //GETING LENGTH OF THE WORD WE'RE TRYING TO GUESS
     var wordGuess = wordToGuess.length;
     console.log(wordGuess);
-
-    x = [wordGuess];
-    //console.log(x);
-
-    //times has been win
-    var wins = '';
-    this.console.log(wins);
-    var space = wordToGuess.split('');
-    this.console.log(space);
-
-
-    //hidde word and show only length as spaces!!!
-
-
-
+    
+    var wins = "";
+    var space = wordToGuess.split("  ");
+    
     //adding number Guesses Remaining to html
     var guessesRemaining = document.getElementById("guessesRemaining");
     guessesRemaining.textContent = numberGuessesRemaining;
-
-
-    //PUT HIDDEN WORD IN HTML ELEMENT
-
-
+    
 
     //on event key up get letter, lower the case and store on var letter
     document.onkeyup = function (event) {
         //getting key pressed and lower
         var letter = event.key.toLowerCase();
-
+        
         if (wordToGuess.indexOf(letter) > -1) {
-            //guesses.innerHTML = "-";
-            guesses.push(letter);
-
+            
+            guesses += letter;
             //show letters alredy guesses
             divGuesses.textContent = guesses;
-            //divGuesses.innerHTML;
+            divGuesses.innerHTML;
             //sustract 1 from number Guesses Remaining (15)
             numberGuessesRemaining--;
             document.getElementById("guessesRemaining").innerHTML = numberGuessesRemaining;
             console.log(numberGuessesRemaining);
-            //numberGuessesRemaining.innerHTML
-            if (guessesRemaining == 0) {
-                alert("Game Over!!");
-            } else {
-                guessesRemaining--;
-                alert("You have meet all intents");
-            }
+            
+            //show letters alredy guesses
+            guesses.textContent += guesses;
+            guesses.innerHTML;
+            
         } else {
             numberGuessesRemaining--;
             document.getElementById("guessesRemaining").innerHTML = numberGuessesRemaining;
             console.log(numberGuessesRemaining);
         }
-
         for (var i = 0; i < wordGuess; i++) {
             if (letter === wordToGuess[i]) {
-                space.innerHTML = wordToGuess[i];
+                space += wordToGuess[i];
+                space.innerHTML;
                 console.log(space);
 
                 //sustract 1 from number Guesses Remaining
@@ -90,10 +73,15 @@ window.onload = function () {
                     wins.innerHTML = "You Win!";
                 }
                 if (numberGuessesRemaining < 1) {
-                    numberGuessesRemaining.innerHTML;
+                    //guessesRemaining.innerHTML =;
                     alert("Game Over");
                 }
             }
         }
     }
 }
+
+//unfortunally i could not made it word like should be
+//sustract 1 from number Guesses Remaining is not working like should be
+//wins are not displaying
+// work underline are not displaying for word selected from game array
