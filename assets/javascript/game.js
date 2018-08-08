19e26f6b324c574d1b8dc04c1eeb88fee2916310
Yeiska <yeiska83@yahoo.com>
2018-08-08 14:39:30 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-
+    
     var game = ["electricity", "current", "tension", "alternator", "battery"];
     var guesses = [];//Stored guesses
     var numberGuessesRemaining = 15;//count guesses
+    var wins = 0;
 
     //hidde word and show only length as spaces!!!
     var space = document.getElementById("space");
-    // space += wordGuess;
-    // space.innerHTML;
+
 
     //Seleccting word to quess from array game
     var wordToGuess = game[Math.floor(Math.random() * game.length)];
@@ -15,78 +15,68 @@ window.onload = function () {
 
     var hiddenWord = "";
 
-    for ( var i = 0; i < wordToGuess.length; i++){
+    for (var i = 0; i < wordToGuess.length; i++) {
         hiddenWord += "_";
     }
 
-    space.textContent = hiddenWord;
+    //space.append(hiddenWord);
+
     var hiddenWordArray = hiddenWord.split("");
+    space.textContent = hiddenWordArray.join("")
+
     //GETING LENGTH OF THE WORD WE'RE TRYING TO GUESS
-    
-    var wins = "";
-    
+
     //adding number Guesses Remaining to html
     var guessesRemaining = document.getElementById("guessesRemaining");
     guessesRemaining.textContent = numberGuessesRemaining;
-    
+
 
     //on event key up get letter, lower the case and store on var letter
     document.onkeyup = function (event) {
         //getting key pressed and lower
-        var letter = event.key.toLowerCase();
-        
+        var letter = event.key.toLowerCase() + '';
+        guesses = letter + " ";
+        divGuesses.append(guesses);
+
+
         if (wordToGuess.indexOf(letter) > -1) {
-           for (var i = 0; i < wordToGuess.length; i++) {
+            for (var i = 0; i < wordToGuess.length; i++) {
                 if (letter === wordToGuess[i]) {
-                    console.log(hiddenWordArray);
+                    // console.log(hiddenWordArray);
                     hiddenWordArray[i] = letter;
                     space.textContent = hiddenWordArray.join("");
                 }
-                // for (var i = 0; i < guesses.length; i++) {
-                //     if (numberGuessesRemaining + space === guesses.length) {
-                //         wins.innerHTML = "You Win!";
-                //     }
-                //     if (numberGuessesRemaining < 1) {
-                //         //guessesRemaining.innerHTML =;
-                //         alert("Game Over");
-                //     }
-                // }
+
             }
         } else {
             numberGuessesRemaining--;
             guessesRemaining.innerHTML = numberGuessesRemaining;
             console.log(numberGuessesRemaining);
+            if (numberGuessesRemaining === 0) {
+                alert("Game Over, Try again");
+                numberGuessesRemaining += 15;
+                reset();
+            }
+            if (wordToGuess.length === hiddenWordArray.length) {
+                //console.log(wordToGuess[i].length);
+                //console.log(hiddenWordArray[i].length);
+                wins++;
+                won.append(wins);
+                alert("You Win!");
+                // guesses = [];
+                // hiddenWord = "";
+                // numberGuessesRemaining = 0;
+                reset();
+            }
         }
-        // for (var i = 0; i < wordGuess; i++) {
-        //     if (letter === wordToGuess[i]) {
-        //         space += wordToGuess[i];
-        //         space.innerHTML;
-        //         console.log(space);
-
-        //         //sustract 1 from number Guesses Remaining
-        //         numberGuessesRemaining--;
-        //         numberGuessesRemaining.innerHTML;
-
-        //     } else {
-        //         guesses += letter;
-        //         guesses.innerHTML;
-        //         numberGuessesRemaining--;
-        //         numberGuessesRemaining.innerHTML;
-        //     }
-        //     for (var i = 0; i < guesses.length; i++) {
-        //         if (numberGuessesRemaining + space === guesses.length) {
-        //             wins.innerHTML = "You Win!";
-        //         }
-        //         if (numberGuessesRemaining < 1) {
-        //             //guessesRemaining.innerHTML =;
-        //             alert("Game Over");
-        //         }
-        //     }
-        // }
     }
+        
 }
+function reset() {
 
-//unfortunally i could not made it word like should be
-//sustract 1 from number Guesses Remaining is not working like should be
-//wins are not displaying
-// work underline are not displaying for word selected from game array
+    hiddenWord = "";
+    guesses = [];
+    numberGuessesRemaining = 15;
+    guessesRemaining = document.getElementById("guessesRemaining");
+    guessesRemaining.textContent = numberGuessesRemaining;
+}
